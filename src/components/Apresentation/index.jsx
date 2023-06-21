d6926c61c35c1db869ee7ef592bbc76cd059e17e
Lucas Anacleto <lucasanacleto04@gmail.com>
2023-06-21 17:34:02 -0300

    Alterando componente de apresentação

--- src/components/Apresentation/index.jsx
+++ src/components/Apresentation/index.jsx
@@ -6,8 +6,8 @@ const Apresentation = () => {
     <div className="container01">
       <section>
         <div className="text-apresentation">
-          <h1 data-aos="fade-right" data-aos-duration="2000">Venda todos os dias, por meio de um Atendimento Descomplicado</h1>
-          <p data-aos="fade-left" data-aos-duration="2000">A nova tendência dos escritórios em 2023 reúne <span>Copywriting, psicologia e inteligência artificial</span> para você transformar qualquer atendimento em <span>vendas</span> rapidamente. </p>
+          <h1 >Venda todos os dias, por meio de um Atendimento Descomplicado</h1>
+          <p >A nova tendência dos escritórios em 2023 reúne <span>Copywriting, psicologia e inteligência artificial</span> para você transformar qualquer atendimento em <span>vendas</span> rapidamente. </p>
         </div>
         <img src="/astro-on-ship.png" alt="Astronauta na Nave" />
       </section>
