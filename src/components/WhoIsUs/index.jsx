37b04d93b05c29146c0f28cf1e6b3cf71eadde48
Lucas Anacleto <lucasanacleto04@gmail.com>
2023-06-27 16:10:27 -0300

    Adicionando COdigo de UTM e ajustando imagem distorcida

--- src/components/WhoIsUs/index.jsx
+++ src/components/WhoIsUs/index.jsx
@@ -6,7 +6,7 @@ const WhoIsUs = () => {
       <h1>Conheça a responsável pelo Atendimento Descomplicado</h1>
       <section>
         <div className="content">
-          <p>Olá, somos a Itellivision!</p>
+          <p>Olá, somos a Intellivision!</p>
           <p> <span>Empresa responsável pela criação de produtos que guiam outras empresas, prestadores de serviço e empreendedores</span> a performarem melhor na área de vendas e relacionamento em mensagens , por meio de <span>copywriting, psicologia e inteligência artificial</span>.</p>
           <p>Possuímos uma equipe qualificada de profissionais especialistas em <span>marketing, vendas e tecnologia</span>.</p>
           <p>O nosso ideal é facilitar <span>resultados em um curto espaço de tempo</span> com uma mistura sofisticada de técnicas milenares e tecnologias de pontas de <span>forma pratica e aplicável no dia a dia</span> por qualquer profissional.</p>
